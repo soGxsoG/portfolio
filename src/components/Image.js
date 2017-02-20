57e5153a381de4afba24cb01dbd60a3ed83df522
SerPRY <privala@bk.ru>
2017-02-20 22:24:43 +0200

    v0.0.2 fixed image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 
 const Image = (props)=>{
-    let path =`` ;
     return(
-        <img className="project__img" src={process.env.PUBLIC_URL+'/langs/'+props.img+'.png'}  alt="project" />
+        <img className="project__img" src={process.env.PUBLIC_URL+'/langs/'+props.img.toLowerCase()+'.png'}  alt="project" />
     );
 }
 
